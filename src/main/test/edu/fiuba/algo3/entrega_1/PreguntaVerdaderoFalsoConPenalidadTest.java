0826c43596b1c15f104c9e9ec5a42b71a1cf5c7b
mauri <mgiannantonio@fi.uba.ar>
2024-06-02 17:31:52 -0300

    Subo las clases creadas de los test de semana1

--- src/main/test/edu/fiuba/algo3/entrega_1/PreguntaVerdaderoFalsoConPenalidadTest.java
+++ src/main/test/edu/fiuba/algo3/entrega_1/PreguntaVerdaderoFalsoConPenalidadTest.java
@@ -2,9 +2,10 @@ package edu.fiuba.algo3.entrega_1;
 
 import edu.fiuba.algo3.modelo.Message;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MessageTest {
+public class PreguntaVerdaderoFalsoConPenalidadTest {
     @Test
     public void messageGreeting() {
         Message message = new Message("Hola Mundo!", "Hello world!");
