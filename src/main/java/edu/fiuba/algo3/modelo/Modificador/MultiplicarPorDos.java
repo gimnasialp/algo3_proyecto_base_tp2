dea2047b4023a4fb977ea489c57aec8e623f3f00
mauri <mgiannantonio@fi.uba.ar>
2024-06-16 20:37:02 -0300

    CAMBIO VARIABLES DE LAS CLASES

--- src/main/java/edu/fiuba/algo3/modelo/Modificador/MultiplicarPorDos.java
+++ src/main/java/edu/fiuba/algo3/modelo/Modificador/MultiplicarPorDos.java
@@ -3,8 +3,11 @@ package edu.fiuba.algo3.modelo.Modificador;
 import java.util.ArrayList;
 
 public class MultiplicarPorDos implements Modificador{
+
+    private final int MULTIPLICADOR = 2;
+
     @Override
     public void aplicar(ArrayList<Integer> puntosRonda, int jugadorIndice) {
-        puntosRonda.set(jugadorIndice, puntosRonda.get(jugadorIndice)*2);
+        puntosRonda.set(jugadorIndice, puntosRonda.get(jugadorIndice)*MULTIPLICADOR);
     }
 }
