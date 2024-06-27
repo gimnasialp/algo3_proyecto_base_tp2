9a9b6b47115d9b11546dbd448a2f632b78b49594
migue.centellas@gmail.com <migue.centellas@gmail.com>
2024-06-27 15:42:31 -0300

    quito clases innecesarias

--- src/main/java/edu/fiuba/algo3/modelo/Partida/OpcionUnica.java
+++
@@ -1,9 +0,0 @@
-package edu.fiuba.algo3.modelo.Partida;
-
-public class OpcionUnica extends Opcion{
-
-    private String textorespuesta;
-    public OpcionUnica(String textoRespuesta) {
-        this.textorespuesta = textoRespuesta;
-    }
-}
