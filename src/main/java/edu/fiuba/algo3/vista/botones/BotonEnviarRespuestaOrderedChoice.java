89e9e223ecf0fc308aebd6860ba659e4f6a2cec2
mauri <mgiannantonio@fi.uba.ar>
2024-07-03 21:57:28 -0300

    agrego

--- src/main/java/edu/fiuba/algo3/vista/botones/BotonEnviarRespuestaOrderedChoice.java
+++ src/main/java/edu/fiuba/algo3/vista/botones/BotonEnviarRespuestaOrderedChoice.java
@@ -15,41 +15,39 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
 import java.util.ArrayList;
-import java.util.List;
+
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+
+import java.util.ArrayList;
 
 public class BotonEnviarRespuestaOrderedChoice extends Button {
 
     public BotonEnviarRespuestaOrderedChoice(ControladorEnviarOrderedChoice controlador, VBox cajaOpciones) {
-        /*super.setText("Enviar respuesta");
-        super.setFont(Font.font(Estilos.FUENTE, 30));
-        super.setPadding(new Insets(10));
-        super.setTextFill(Color.BLACK);
-        ArrayList<SpinnerOrderedChoice> opcionesOrden = new ArrayList<>();
-        for (Node opcion : cajaOpciones.getChildren()) {
-            if (opcion instanceof SpinnerOrderedChoice) {
-                opcionesOrden.add((SpinnerOrderedChoice) opcion);
-            }
-        }
-        */
         super.setText("Enviar respuesta");
         super.setFont(Font.font(Estilos.FUENTE, 30));
         super.setPadding(new Insets(10));
         super.setTextFill(Color.BLACK);
-        //Recibo la VBox de opciones dentro de pregunta
-        ObservableList<Node> opciones = cajaOpciones.getChildren();
-        ArrayList<SpinnerOrderedChoice> opcionesOrden = new ArrayList<>();
-        for (Node opcion : opciones) {
-            opcionesOrden.add((SpinnerOrderedChoice) opcion);
-        }
-        System.out.println("esto es dentro del boton enviar respuesta" + opcionesOrden);
-        controlador.agregarOpcionesSeleccionadas(opcionesOrden);
         super.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, Estilos.BORDE_CURVO, Estilos.GROSOR_BORDE)));
         Background unFondo = new Background(new BackgroundFill(Color.web(Estilos.VERDE, Estilos.ALPHA_BOTON_INACTIVO), Estilos.BORDE_CURVO, new Insets(0)));
         super.setBackground(unFondo);
 
-        super.setOnAction(controlador);
+        super.setOnAction(e -> {
+            ObservableList<Node> opciones = cajaOpciones.getChildren();
+            ArrayList<SpinnerOrderedChoice> opcionesOrden = new ArrayList<>();
+            for (Node opcion : opciones) {
+                if (opcion instanceof SpinnerOrderedChoice) {
+                    opcionesOrden.add((SpinnerOrderedChoice) opcion);
+                }
+            }
+            controlador.agregarOpcionesSeleccionadas(opcionesOrden);
+            controlador.handle(e);
+        });
         super.setOnMouseEntered(new ControladorActivarBoton(this, Estilos.VERDE));
         super.setOnMouseExited(new ControladorDesactivarBoton(this, Estilos.VERDE));
     }
-
 }
+
